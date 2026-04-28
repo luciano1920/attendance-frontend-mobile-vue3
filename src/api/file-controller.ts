c4fa3ec097a748ddbce2b4039b01d9f46021b8cc
luciano1920 <1290582790@qq.com>
2026-04-28 17:37:31 +0800

    feat(components): 新增封装图片上传组件
    1、封装一个图片上传组件，统一处理上传时的相关操作，调用后端文件上传接口请求上传

--- src/api/file-controller.ts
+++ src/api/file-controller.ts
@@ -1,21 +1,51 @@
 /**
  * @Author       : 罗钧 71233895@chinatelecom.cn
  * @Date         : 2026-03-24 09:04
- * @LastEditors  : 罗钧 71233895@chinatelecom.cn
- * @LastEditTime : 2026-03-24 14:38
+ * @LastEditors  : luciano1920 1290582790@qq.com
+ * @LastEditTime : 2026-04-27 17:26
  * @FilePath     : \attendance-frontend-mobile\src\api\file-controller.ts
  * @Description  : 文件服务模块-API接口
  */
 import request from '@/libs/axios/request'
 
 /** 文件上传 POST /admin-api/infra/file/upload */
-export async function uploadFileUsingPost(data: any, options?: { [key: string]: any }) {
+export async function uploadFileUsingPost(
+  params: any,
+  body: {},
+  file?: File,
+  options?: { [key: string]: any },
+) {
+  const formData = new FormData()
+
+  if (file) {
+    formData.append('file', file)
+  }
+
+  Object.keys(body).forEach((ele) => {
+    const item = (body as any)[ele]
+
+    if (item !== undefined && item !== null) {
+      if (typeof item === 'object' && !(item instanceof File)) {
+        if (item instanceof Array) {
+          item.forEach((f) => formData.append(ele, f || ''))
+        } else {
+          formData.append(ele, JSON.stringify(item))
+        }
+      } else {
+        formData.append(ele, item)
+      }
+    }
+  })
+
   return request('/admin-api/infra/file/upload', {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
+    params: {
+      ...params,
     },
-    data,
-    ...(options || {})
+    data: formData,
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+    ...(options || {}),
   })
 }
---
+++ src/components/PictureUpload.vue
@@ -0,0 +1,172 @@
+<!--
+ * @Author       : luciano1920 1290582790@qq.com
+ * @Date         : 2026-04-27 16:35
+ * @LastEditors  : luciano1920 1290582790@qq.com
+ * @LastEditTime : 2026-04-28 16:25
+ * @FilePath     : \attendance-frontend-mobile\src\components\PictureUpload.vue
+ * @Description  :
+-->
+<template>
+  <div class="picture-upload">
+    <t-upload
+      v-model="files"
+      :action="actionUrl"
+      :headers="uploadHeaders"
+      :max="maxCount"
+      :size-limit="{ size: 8, unit: 'MB', message: '图片大小不能超过 {sizeLimit} MB' }"
+      :accept="acceptFileType"
+      multiple
+      allow-upload-duplicate-file
+      :before-upload="beforeUpload"
+      :on-one-file-fail="handleFailUpload"
+      :on-success="handleSuccessUpload"
+      :on-remove="handleRemove"
+      :on-validate="handleValidate"
+      :format-response="handleFormatResponse"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import {
+  Message,
+  type FormatResponseContext,
+  type SuccessContext,
+  type UploadFile,
+  type UploadRemoveContext,
+  type UploadValidateType,
+} from 'tdesign-mobile-vue'
+
+import { useUserStore } from '@/stores/user-store'
+import { getTokenUtil } from '@/utils/auth'
+
+const userStore = useUserStore()
+
+interface Props {
+  // 允许上传的最大图片数量
+  maxCount?: number
+  // 上传成功的回调函数
+  successUpload?: (fileList: any) => void
+  // 移除上传的图片的回调函数
+  removeUpload?: (fileList: any) => void
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  maxCount: 3,
+})
+
+// 允许上传的文件类型，图片上传组件默认为 JPEG、PNG、JPG 三种格式，如果需要上传其他格式，后续可以修改为 Props
+const acceptFileType = ref<string>('image/jpeg,image/png,image/jpg')
+// 上传接口地址
+const actionUrl = import.meta.env.VITE_API_BASE_URL + '/admin-api/infra/file/upload'
+// 上传的请求头
+const uploadHeaders = computed(() => {
+  const token = userStore.loginUser.accessToken ?? getTokenUtil()
+  return {
+    Authorization: token ?? '',
+  }
+})
+
+// 上传的文件列表
+const files = ref<UploadFile[]>([])
+
+/**
+ * 上传前的校验
+ * @param file 上传的文件
+ */
+const beforeUpload = (file: UploadFile) => {
+  const acceptTypeList = acceptFileType.value.split(',')
+
+  if (!acceptTypeList.includes(file.raw?.type || '')) {
+    return false
+  }
+}
+
+/**
+ * 上传文件校验
+ * @param context 校验的上下文
+ */
+const handleValidate = (context: { type: UploadValidateType; files: UploadFile[] }) => {
+  if (context.type === 'FILE_OVER_SIZE_LIMIT') {
+    Message.error({
+      content: context.files[0]?.response?.error,
+      offset: [10, 16],
+    })
+  }
+
+  if (context.type === 'FILES_OVER_LENGTH_LIMIT') {
+    Message.error({
+      content: `最多上传 ${props.maxCount} 张图片`,
+      offset: [10, 16],
+    })
+  }
+
+  if (context.type === 'CUSTOM_BEFORE_UPLOAD') {
+    Message.error({ content: '上传文件类型错误，请上传图片', offset: [10, 16] })
+  }
+}
+
+/**
+ * 格式化上传后的响应值
+ * @param response 上传后的响应值
+ * @param context 格式化响应的上下文
+ */
+const handleFormatResponse = (response: any, context: FormatResponseContext) => {
+  // 如果是上传成功，则返回上传后的文件地址，否则返回错误信息
+  if (response.code === 0) {
+    return {
+      ...response,
+      url: response.data,
+    }
+  } else {
+    return {
+      ...response,
+      error: response.msg,
+    }
+  }
+}
+
+/**
+ * 上传成功的回调函数
+ * @param successContext 上传成功的上下文
+ */
+const handleSuccessUpload = (successContext: SuccessContext) => {
+  if (successContext.response[0].code === 0) {
+    Message.success({ content: '上传成功', offset: [10, 16] })
+  }
+  // 把上传的最新文件列表回调给父组件，也可以直接采用 files.value，因为 files.value 已经被同步修改了
+  props.successUpload?.(successContext.fileList)
+}
+
+/**
+ * 上传失败的回调
+ * @param file 上传失败的文件
+ */
+const handleFailUpload = (file: UploadFile) => {
+  if (file.response?.code !== 0) {
+    Message.error({
+      content: '上传失败，' + file.response?.msg,
+      offset: [10, 16],
+    })
+  }
+}
+
+/**
+ * 移除上传的文件
+ * @param removeContext 移除文件的上下文
+ */
+const handleRemove = (removeContext: UploadRemoveContext) => {
+  if (removeContext.file) {
+    Message.success({ content: '移除文件成功', offset: [10, 16] })
+  }
+  // 移除文件组件会自行同步修改本地的 files，直接回调给父组件即可
+  props.removeUpload?.(files.value)
+}
+</script>
+
+<style scoped lang="scss">
+.picture-upload {
+  --td-upload-grid-columns: 3;
+}
+</style>
